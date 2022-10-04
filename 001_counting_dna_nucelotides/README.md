147f3cd72ecadfd2eadc3e9ae0505e29c1f19d62
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-10-04 17:22:58 -0400

    add exercise 1

---
+++ 001_counting_dna_nucelotides/README.md
@@ -0,0 +1,2 @@
+Counting number of bases in a DNA sequence.<br>
+Order: A, C, G, T.
---
+++ 001_counting_dna_nucelotides/solution1.py
@@ -0,0 +1,19 @@
+fname = 'rosalind_dna.txt'
+f = open(fname,'r')
+seq = f.read()
+f.close()
+
+def count_dna(x):
+    dna = x.upper()
+    A = str(dna.count('A'))
+    C = str(dna.count('C'))
+    G = str(dna.count('G'))
+    T = str(dna.count('T'))
+    return A + ' ' + C + ' ' + G + ' ' + T
+
+output = count_dna(x = seq)
+
+with open('output.txt', 'w') as f:
+    f.write(output)
+
+print(output)
